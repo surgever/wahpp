d9a4472f004de79772e074bf806352f03f863661
surgever <surgever.com@gmail.com>
2013-08-21 02:04:07 +0200

    Trabajo en splash

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,19 @@
+navigator.splashscreen.hide();
+setTimeout(function(){alert('Hello Wah!');},800);
 
+var plegar = function() {
+	var rel= $(this).attr('rel'),
+		bloqueid= '#'+rel;
+	if($('#acordeon').hasClass(rel)) {
+		$(bloqueid).slideDown('slow', function() {
+			$('#acordeon').removeClass(rel);
+		});
+	} else {
+		$(bloqueid).slideUp('slow', function() {
+			$('#acordeon').addClass(rel);
+		});
+	}
+};
+$(document).ready(function() {
+	$('#wahlogo').css('background','red');
+});
