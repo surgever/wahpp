f9e6d26293c1073b8dd45bbff911a7f8a93c1718
surgever <surgever.com@gmail.com>
2013-08-21 03:44:41 +0200

    Ajustando temporización
    Ahora que funciona jquery, colocamos algunos settimeout para asegurar
    que los efectos son visibles.

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,12 @@
-var red = function(event) {
-	event.preventDefault();
-	$('#wahlogo').css('background','red');
-};
-$(document).ready(function() {
-	$('#wahlogo').click(red);
+var carga = function(event) {
 	$('#page-load div').fadeIn(5000,function(){
 		$('span',this).animate({opacity:1},function(){
-			$('#page-load').slideUp();
+			setTimeout(function(){
+				$('#page-load').fadeOut();
+			},8000);
 		});
 	});
-	setTimeout(function(){alert('Hello Wah!');},8000);
+};
+$(document).ready(function() {
+	setTimeout(carga,5000);
 });
