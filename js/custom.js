4c7795d7c88ca06c00a784337d510506c2720b06
surgever <surgever.com@gmail.com>
2013-08-21 03:59:00 +0200

    Visibilizado multicolor

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+$('html').css('background','orange');
 var carga = function(event) {
 	$('#page-load div').fadeIn(5000,function(){
 		$('span',this).animate({opacity:1},function(){
@@ -8,5 +9,8 @@ var carga = function(event) {
 	});
 };
 $(document).ready(function() {
+	$('html').css('background','yellow');
+	setTimeout(function(){$('html').css('background','lime');},2000);
+	setTimeout(function(){$('html').css('background','blue');},5000);
 	setTimeout(carga,5000);
 });
