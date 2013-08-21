92bc23e4e788a6903b0972a995429e7bc6c14ea6
surgever <surgever.com@gmail.com>
2013-08-21 02:49:29 +0200

    En doc ready splashscreen.hide

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,9 @@
-/*navigator.splashscreen.hide();*/
-
 var red = function(event) {
 	event.preventDefault();
 	$('#wahlogo').css('background','red');
 };
 $(document).ready(function() {
+	navigator.splashscreen.hide();
 	$('#wahlogo').click(red);
 	$('#page-load div').fadeIn(2000,function(){
 		$('span',this).animate({opacity:1},function(){
