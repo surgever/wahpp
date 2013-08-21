0cc8210549d53fefa9536c4eee0e96d0cbd55c2b
surgever <surgever.com@gmail.com>
2013-08-21 03:24:38 +0200

    Reactivado splashscreen.hide

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,12 @@
+navigator.splashscreen.hide();
+
 var red = function(event) {
 	event.preventDefault();
 	$('#wahlogo').css('background','red');
 };
 $(document).ready(function() {
-	$('#pCargando').remove();
 	$('#wahlogo').click(red);
-	$('#page-load div').fadeIn(2000,function(){
+	$('#page-load div').fadeIn(5000,function(){
 		$('span',this).animate({opacity:1},function(){
 			$('#page-load').slideUp();
 		});
