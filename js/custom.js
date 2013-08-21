efc8f3866205d602eaab5f9cda7c0ed656eea459
surgever <surgever.com@gmail.com>
2013-08-21 04:19:35 +0200

    Desvisibilizado: vuelta al negro

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,3 @@
-$('html').css('background','orange');
 var carga = function(event) {
 	$('#page-load div').fadeIn(5000,function(){
 		$('span',this).animate({opacity:1},function(){
@@ -9,8 +8,5 @@ var carga = function(event) {
 	});
 };
 $(document).ready(function() {
-	$('html').css('background','yellow');
-	setTimeout(function(){$('html').css('background','lime');},2000);
-	setTimeout(function(){$('html').css('background','blue');},5000);
-	setTimeout(carga,5000);
+	carga();
 });
