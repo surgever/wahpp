26613ddd152e7de82f4450594a5c6b2a1f0a1a49
surgever <surgever.com@gmail.com>
2013-09-12 00:06:15 +0200

    App tareas: nueva tarea y eventos touch

--- app/tareas.php
+++ app/tareas.php
@@ -1,6 +1,7 @@
-<div id="tareaswrapper" ontouchstart="touchStart(event,'tareaswrapper');"  ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);" >
+<div id="tareaswrapper">
 <ul id="tareas">
 </ul>
+<button id="crear">Añadir tarea</button>
 </div>
 <script type="text/javascript" src="js/swipesensejs.js" />
 <script type="text/javascript">
@@ -11,14 +12,26 @@ var f5lista = function() {
 	tareas = JSON.parse(localStorage.tareas);
 	var html='';
 	for (var i=0;i<tareas.length;i++) {
-		html += '<li class="'+tareas[i][0]+' '+tareas[i][1]+'" id="li'+i+'" ontouchstart="touchStart(event,\'tareaswrapper\');"  ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);"><i></i><b></b><h3>'+tareas[i][2]+'</h3><span>'+tareas[i][3]+'</span></li>';
+		html += '<li class="'+tareas[i][0]+' '+tareas[i][1]+'" id="li'+i+'" ><i></i><b></b><h3>'+tareas[i][2]+'</h3><span>'+tareas[i][3]+'</span></li>';
 	}
 	lista.html(html);
 	li=$('li',lista );
 	eventostareas();
 };
 var tareanueva = function(o) {
-	return false;};
+	tarea = ['','white'];
+	var tit = '';
+	if(tit = prompt('Tarea:')) {
+	while(tit==false) tit = prompt('Tarea');
+	console.log(tit);
+	tarea[2] = tit;
+	if(confirm('¿Quieres poner una descripción a esta tarea?'))
+		tarea[3] = prompt('Descripción');
+		else tarea[3] = '';
+	tareas.push(tarea);
+	localStorage.tareas = JSON.stringify(tareas);
+	f5lista();
+	}};
 var tareaver = function(e) {
 	$(this).parent('li').toggleClass('show');};
 var tareaeditar = function(e) {
@@ -28,12 +41,14 @@ var tareahecho = function(e) {
 	if(tarea.hasClass('hecho')) {tarea.removeClass('hecho');tareas[id][0]='';}
 		else  {tarea.addClass('hecho');tareas[id][0]='hecho';}
 	localStorage.tareas = JSON.stringify(tareas);
+	f5lista();
 	};
 var tareaborrar = function(e) {
 	tarea  = $(this).parent('li'); id = tarea[0].id.substr(2);
 	$(this).parent('li').remove();
 	tareas.splice(id, 1);
 	localStorage.tareas = JSON.stringify(tareas);
+	f5lista();
 	};
 var tareacolorear = function() {
 	tarea  = $(this).parent('li'); id = tarea[0].id.substr(2);
@@ -44,27 +59,32 @@ var tareacolorear = function() {
 		break; break;
 	}
 	localStorage.tareas = JSON.stringify(tareas);
+	f5lista();
 	};
 var eventostareas = function() {
-	$('h3',li).click(tareaver);
-	$('h3',li).dblclick(tareaeditar);
+	$(li).on('touchstart',function(e){touchStart(e,$(this).attr('id'));});
+	$(li).on('touchend',function(e){touchEnd(e);});
+	$(li).on('touchmove',function(e){touchMove(e);});
+	$(li).on('touchcancel',function(e){touchCancel(e);});
+	/*	$('h3',li).click(tareaver);
+	$('h3',li).dblclick(tareaeditar);*/
 	$('i',li).dblclick(tareahecho);
 	$('i',li).click(tareacolorear);
 	$('b',li).click(tareaborrar);
 };
 
+	$('#crear').on('click',tareanueva);
 if(!localStorage.tareas) localStorage.tareas = '[["hecho","white","Descargar la Wahpp","Bajarse la aplicación de Wah"],["","white","Empezar un hack","Crear tu propio proyecto"],["","yellow","Publicar el hack en Wah",""]]';
 f5lista();
 
-//if(localStorage.notepad) notepad.html(localStorage.notepad);
 </script>
 <style type="text/css">
 #pageapp.on {background: #ccc}
 #tareaswrapper {clear:both; margin:10px; background:#4586d6;padding:30px 10px 5px;color:#fff;min-height:400px; line-height: 20px;box-shadow: inset 0px 5px 10px #174292, 0px -1px 0px #999, 0px 1px 2px #fff; border-radius: 5px}
 #tareas li {min-height:30px;clear:both; background:rgba(0,0,0,0.2); border-radius:5px; margin:10px 0}
 #tareas li *{display:block; height:30px;}
-#tareas i {width: 60px; float: left; background: lightgreen; opacity:0.1}
-#tareas b {width: 60px; float: right; background: red; opacity:0.1}
+#tareas i {width: 60px; float: left; }
+#tareas b {width: 60px; float: right; }
 #tareas h3 {padding: 5px 0; font-weight: bold}
 #tareas h3, #tareas li.show span {height:auto; }
 #tareas span {height:0px; overflow: hidden}
